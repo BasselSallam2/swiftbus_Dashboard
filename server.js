de8bf9ee51b16b3d9daafd2372ac09af61e50cda
Bassel Sallam <Bassela.sallam@gmail.com>
2025-11-15 16:48:42 +0200

    dashboard v5.7

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use(WhatsRouter) ;
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(PORT, () => {
-	
+	console.log(`Server running on port ${PORT}`);
 });
 
 process.on("unhandledRejection", (err) => {
